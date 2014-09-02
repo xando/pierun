cfcf241372941690a7b9b5f559e1f837451b9410
xando <sebastian.pawlus@gmail.com>
2014-09-02 13:05:30 +0200

    update

--- thor/bin.py
+++ thor/bin.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 import sys
 import docker
@@ -75,6 +76,51 @@ def build_image(name):
         print(i)
 
 
+def get_container(name, all=True):
+    name = _name(name)
+    for c in DOCKER.containers(all=all):
+        if "/%s" % name in c['Names']:
+            return DOCKER.inspect_container(c)
+
+    return None
+
+
+def umount_share(mount_path):
+    file_system_path = os.path.join(mount_path, '.file-system')
+
+    subprocess.Popen(
+        "fusermount -u %s -z" % file_system_path,
+        shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ).communicate()
+
+    try:
+        os.rmdir(file_system_path)
+    except OSError:
+        pass
+
+
+def mount_share(host_address, mount_path):
+    umount_share(mount_path)
+
+    file_system_path = os.path.join(mount_path, '.file-system')
+
+    try:
+        os.mkdir(file_system_path)
+    except OSError:
+        pass
+
+    p = subprocess.Popen(
+        "sshfs -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o password_stdin root@%s:/ %s" % (
+            host_address,
+            file_system_path
+        ),
+        stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+        shell=True,
+    )
+    p.stdin.write("test")
+
+
+
 def THOR_create(args):
     name = _name(args.name)
 
@@ -91,7 +137,8 @@ def THOR_create(args):
     build_image(name)
 
     DOCKER.create_container(
-        name, detach=True,
+        name,
+        detach=True,
         ports=[22],
         volumes=[
             '/v/',
@@ -100,43 +147,64 @@ def THOR_create(args):
         name=name,
     )
 
+    DOCKER.start(
+        name,
+        binds={
+            os.getcwd(): {
+                'bind': '/v',
+                'ro': False
+            },
+            SSH_KEY: {
+                'bind': '/root/.ssh/authorized_keys',
+                'ro': True
+            },
+        },
+        privileged=True
+    )
 
-def THOR_activate(args):
+    container = get_container(args.name)
+
+    mount_share(container['NetworkSettings']['IPAddress'], container['Volumes']['/v'])
+
+
+def THOR_go(args):
+    c = get_container(args.name)
+
+    host = c['NetworkSettings']['IPAddress']
+
+    subprocess.Popen(
+        "ssh "
+        "-o UserKnownHostsFile=/dev/null "
+        "-o StrictHostKeyChecking=no "
+        "-o GSSAPIAuthentication=no "
+        " root@%s -t \"cd /v; bash --login\"" % host,
+        shell=True,
+    ).communicate()
+
+
+def THOR_start(args):
+    c = get_container(args.name)
+
+    if not c:
+        return None
+
+    DOCKER.start(c['Id'])
+
+    container = get_container(args.name)
+
+    mount_share(container['NetworkSettings']['IPAddress'], container['Volumes']['/v'])
+
+
+def THOR_stop(args):
     name = _name(args.name)
 
     for c in DOCKER.containers(all=True):
         if "/%s" % name in c['Names']:
-
-            ssh_port = 49170
-
-            DOCKER.start(
-                c['Id'],
-                port_bindings={
-                    22: ssh_port
-                },
-                binds={
-                    os.getcwd(): {
-                        'bind': '/v',
-                        'ro': False
-                    },
-                    SSH_KEY: {
-                        'bind': '/root/.ssh/authorized_keys',
-                        'ro': True
-                    },
-                },
-                privileged=True
-            )
-
-            subprocess.Popen(
-                "ssh "
-                "-o UserKnownHostsFile=/dev/null "
-                "-o StrictHostKeyChecking=no "
-                "-o GSSAPIAuthentication=no "
-                " root@localhost -p %s -t \"cd /v; bash --login\"" % ssh_port,
-                shell=True,
-            ).communicate()
-
             DOCKER.stop(c['Id'])
+
+            container = get_container(args.name)
+
+            umount_share(container['Volumes']['/v'])
 
 
 def THOR_list(args):
@@ -144,10 +212,13 @@ def THOR_list(args):
     for c in containers:
         if c['Image'].startswith('%s-' % PREFIX):
             name = c['Names'][0].strip('/%s-' % PREFIX)
-            print name
+            if c['Status'].startswith('Up') and args.status in ['all', 'running']:
+                print name
+            if c['Status'].startswith('Exited') and args.status in ['all', 'stopped']:
+                print name
 
 
-def THOR_destroy(args):
+def THOR_remove(args):
     name = _name(args.name)
 
     for c in DOCKER.containers(all=True):
@@ -156,7 +227,6 @@ def THOR_destroy(args):
             DOCKER.stop(c['Id'])
             DOCKER.remove_container(c['Id'])
             DOCKER.remove_image(name)
-
 
 
 def main():
@@ -169,19 +239,28 @@ def main():
 
     list_command = subparsers.add_parser('list', help='list')
     list_command.set_defaults(func=THOR_list)
+    list_command.add_argument('status', nargs='?', default="all", type=str, help='status')
 
-    destroy_command = subparsers.add_parser('destroy', help='destroy')
-    destroy_command.add_argument('name', type=str, help='name')
-    destroy_command.set_defaults(func=THOR_destroy)
+    remove_command = subparsers.add_parser('remove', help='destroy')
+    remove_command.add_argument('name', type=str, help='name')
+    remove_command.set_defaults(func=THOR_remove)
 
-    activate_command = subparsers.add_parser('activate', help='activate')
-    activate_command.add_argument('name', type=str, help='name')
-    activate_command.set_defaults(func=THOR_activate)
+    go_command = subparsers.add_parser('go', help='go')
+    go_command.add_argument('name', type=str, help='name')
+    go_command.set_defaults(func=THOR_go)
+
+    start_command = subparsers.add_parser('start', help='stop')
+    start_command.add_argument('name', type=str, help='name')
+    start_command.set_defaults(func=THOR_start)
+
+    stop_command = subparsers.add_parser('stop', help='stop')
+    stop_command.add_argument('name', type=str, help='name')
+    stop_command.set_defaults(func=THOR_stop)
 
     args = parser.parse_args()
     args.func(args)
 
 
 if __name__ == "__main__":
-    main()
 
+    main()
